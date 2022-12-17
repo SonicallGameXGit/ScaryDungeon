9e66eff9ec00aaa11c6bfac59d1cd8eb29df1d38
SonicallGameX <116002303+SonicallGameXGit@users.noreply.github.com>
2022-12-17 11:08:30 +0100

    Delete Source directory

--- Source/src/org/sgx/sc/game/math/TimeUtil.java
+++
@@ -1,7 +0,0 @@
-package org.sgx.sc.game.math;
-
-public class TimeUtil {
-    public static double smooth(double a, double b, double smoothness) {
-        return smoothness == 0.0 ? a : (b - a) / smoothness;
-    }
-}
